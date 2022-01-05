dd26645e9f69b671f7e4cd8cd003d38b74a29661
Tobias Kaiser <ubixh@student.kit.edu>
2022-01-05 18:36:18 +0100

    create gallery component

---
+++ src/views/Gallery.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<script>
+import { Vue } from "vue-class-component";
+
+export default class Gallery extends Vue {
+     
+}
+</script>
+
+<style scoped>
+
+</style>
